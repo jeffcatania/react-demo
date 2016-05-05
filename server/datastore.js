a65e7206ce171d098eae0430a757c204acb5b50b
Ian Sinnott <ian@iansinnott.com>
2016-05-05 15:00:35 -0700

    Add comments to datastore

--- server/datastore.js
+++ server/datastore.js
@@ -6,19 +6,40 @@ const uuid = require('node-uuid').v4;
 const data = Object.create(null);
 
 /**
- * NOTE: We use a bunch of clone deep to ensure that no references to the data
+ * @module DataStore
+ *
+ * A very simple API wrapping a plain JavaScript object. This API is simply
+ * meant to simulate simple database CRUD operations.
+ *
+ * NOTE: We use a bunch of cloneDeep to ensure that no references to the data
  * object or its values are returned and directly mutable.
  */
 
 module.exports = {
+
+  /**
+   * Return all data in our datastore
+   * @return {Array<object>}
+   */
   fetchAll() {
     return cloneDeep(values(data));
   },
 
+  /**
+   * Return a single entry in our datastore by ID
+   * @param {string} id
+   * @return {object}
+   */
   fetch(id) {
     return cloneDeep(data[id]);
   },
 
+  /**
+   * Save a single entry in our datastore and return the newly saved entry,
+   * complete with ID, created and updated properties.
+   * @param {object} v
+   * @return {object}
+   */
   save(v) {
     const id = uuid();
     const result = Object.assign(Object.create(null), v, {
@@ -30,16 +51,28 @@ module.exports = {
     return cloneDeep(result);
   },
 
+  /**
+   * Update an entry within our datastore by its ID. Performs a deep merge.
+   * @param {string} id
+   * @param {object} v
+   * @return {object}
+   */
   update(id, v) {
-    merge(data[id], v, {  // _.merge mutates values
+    merge(data[id], v, {  // NOTE: _.merge mutates values
       updated: Date.now(),
     });
     return cloneDeep(data[id]);
   },
 
-  remove(k) {
-    const d = data[k];
-    delete data[k];
+  /**
+   * Remove a single entry from our datastore and return the newly removed
+   * entry.
+   * @param {string} id
+   * @return {object}
+   */
+  remove(id) {
+    const d = data[id];
+    delete data[id];
     return d;
   },
 };
